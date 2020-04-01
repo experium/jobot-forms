d4005b813dec7619d39ea7ddab3f8cf198a1fa23
Andrey <borisenkoandrei@gmail.com>
2020-04-01 16:12:34 +0300

    Fix styles

---
+++ generateName.js
@@ -0,0 +1,15 @@
+const name = (className, filePath) => {
+    const pathArray = filePath.split('/');
+    const fileName = pathArray[pathArray.length - 1];
+    const isModule = fileName.includes('module');
+
+    if (isModule) {
+        const moduleName = fileName.split('.')[0];
+
+        return `${moduleName}__${className}`
+    } else {
+        return className;
+    }
+}
+
+module.exports = name;
