0d3bb55abb4dda55c6728caa0fe578229dd1e550
borisenkoandrei <borisenkoandrei@gmail.com>
2020-04-02 18:32:39 +0300

    Add vacancy list (#8)

---
+++ demo/components/VacanciesList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { path } from 'ramda';
+import { Link} from 'react-router-dom';
+
+import { getVacancies } from '../queries/vacancies';
+
+class VacanciesList extends Component {
+    render() {
+        const vacancies = path(['data', 'vacancies', 'items'], this.props);
+
+        return vacancies ? (
+            <div className='vacancies-list'>
+                { vacancies.map(({ title, id }) => (
+                    <div className='vacanci-item'>
+                        <div className='vacanci-item-title'>{ title }</div>
+                        <div className='vacanci-item-button'>
+                            <Link to={`/form/${id}`}>Перейти</Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        ) : (
+            <div className='vacancy-placeholder' >Список вакансий пуст</div>
+        );
+    }
+}
+
+export default graphql(getVacancies)(VacanciesList);
