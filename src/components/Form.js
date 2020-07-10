bf33382a4caee5248ef180921d720bc13fd885d1
Andrey <borisenkoandrei@gmail.com>
2020-07-10 12:46:28 +0300

    Add field with mask

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,7 +14,7 @@ import Input from './formComponents/Input';
 import Checkbox, { PersonalDataAgreement, Boolean } from './formComponents/Checkbox';
 import Select from './formComponents/Select';
 import DictionarySelect from './formComponents/DictionarySelect';
-import { PhoneInput } from './formComponents/MaskedInput';
+import MaskedInput, { PhoneInput } from './formComponents/MaskedInput';
 import DateSelect from './formComponents/DateSelect';
 import File from './formComponents/File';
 import '../styles/index.css';
@@ -37,9 +37,10 @@ const CompositeError = ({ meta }) => {
 
 const getFieldComponent = (field, components) => {
     const { type, settings = {} } = field;
+    const mask = prop('mask', settings);
 
     const DEFAULT_FIELDS = {
-        text: Input,
+        text: mask ? MaskedInput : Input,
         email: Input,
         personalDataAgreement: PersonalDataAgreement,
         dictionary: prop('checkboxes', settings) ? (
