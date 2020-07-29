488edc5a7fa00021cd7deab0bc077a3fbed516e1
Andrey <borisenkoandrei@gmail.com>
2020-07-30 00:37:58 +0300

    Fix document accept types

--- src/components/formComponents/File.js
+++ src/components/formComponents/File.js
@@ -17,7 +17,7 @@ const TYPES = {
     audio: 'audio/*',
     video: 'video/*',
     image: 'image/jpeg, image/tiff, image/png',
-    document: 'text/plain, application/msword, application/rtf, application/pdf, text/html, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.oasis.opendocument.text',
+    document: 'text/plain, application/msword, application/rtf, application/pdf, text/html, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.oasis.opendocument.text, .doc, .docx, .xls, .xlsx, .odt, .rtf',
 };
 
 const MEDIA = {
