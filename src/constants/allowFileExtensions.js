c121ced8558e5581eb2c3ead1825cdedb9f1004f
Andrey <borisenkoandrei@gmail.com>
2020-08-31 12:28:54 +0300

    Fix file validation

---
+++ src/constants/allowFileExtensions.js
@@ -0,0 +1,11 @@
+export const TYPES = {
+    audio: 'audio/*',
+    video: 'video/*',
+    image: 'image/jpeg, image/tiff, image/png',
+    document: 'text/plain, application/msword, application/rtf, application/pdf, text/html, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.oasis.opendocument.text, .doc, .docx, .xls, .xlsx, .odt, .rtf, .txt',
+};
+
+export const VALIDATION_FILE_TYPES = {
+    image: 'jpeg, tiff, png',
+    document: 'doc, docx, html, odt, pdf, rtf, xls, xlsx',
+};
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,15 @@
-import { path, split } from 'ramda';
+import { path, split, replace, contains, head, prop } from 'ramda';
 import * as yup from 'yup';
 
 import i18n from './i18n';
+
+import { TYPES, VALIDATION_FILE_TYPES } from '../constants/allowFileExtensions';
+
+export const checkFileType = (fileType, mimeType) => {
+    const allowFileTypes = TYPES[fileType];
+
+    return contains(mimeType, split(',', replace(/\s/g, '', allowFileTypes)));
+};
 
 export const compositeValidator = (value) => {
     return value && (value.length > 0) ? undefined : 'Блок обязателен для заполнения';
@@ -22,13 +30,46 @@ export const validateLink = (field, values) => {
 
 export const validate = (value, form, field, props, fieldsWithoutValidation) => {
     const htmlOpd = path(['htmlOpd'], props);
+    const allowFileExtensions = props.allowFileExtensions;
 
     const rules = {
         email: yup.string().email(i18n.t('errors.email')),
         personalDataAgreement: htmlOpd ? yup.string() : yup.boolean(),
         boolean: yup.boolean(),
         choice: path(['settings', 'multiple'], field) ? yup.array() : yup.string(),
-        file: path(['settings', 'multiple'], field) ? yup.array() : yup.string(),
+        file: allowFileExtensions ? path(['settings', 'multiple'], field) ? yup.array() : yup.string() : (
+            yup.mixed().test({
+                name: 'fileExtensions',
+                message: ({ value }) => {
+                    const types = Array.isArray(value) ? (
+                        prop(prop('type', head(value)), VALIDATION_FILE_TYPES)
+                    ) : prop('type', VALIDATION_FILE_TYPES);
+
+                    return i18n.t('errors.fileType', { types });
+                },
+                test: (value) => {
+                    if (!value) {
+                        return true;
+                    }
+
+                    if (Array.isArray(value)) {
+                        let result = true;
+
+                        value.forEach(value => {
+                            if (result) {
+                                result = checkFileType(value.type, value.contentType);
+                            }
+                        });
+
+                        return result;
+                    } else {
+                        const { type, contentType } = value;
+
+                        return checkFileType(type, contentType);
+                    }
+                },
+            })
+        ),
         money: yup.object().shape({
             amount: field.required ? (
                 yup.number().moreThan(0, ({ more }) => i18n.t('errors.moreThan', { more })).required(() => i18n.t('errors.required'))
