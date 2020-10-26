88a621043ce8dbe941d16e3964f8008e00888a73
Anastasia Tsegelskaya <muaaaaaaa@mail.ru>
2020-10-26 12:32:07 +0300

    add other field for choices

--- src/components/formComponents/Select.js
+++ src/components/formComponents/Select.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactSelect, { components } from 'react-select';
-import { path, contains, find, propEq, filter, findIndex, equals, take, isEmpty } from 'ramda';
+import { path, contains, find, propEq, filter, findIndex, equals, take, isEmpty, includes, pathOr } from 'ramda';
 import { VariableSizeList as List } from 'react-window';
 import qs from 'qs';
 import { withTranslation } from 'react-i18next';
@@ -10,10 +10,19 @@ import '../../styles/select.css';
 import { GEO_DICTIONARIES_TYPES } from '../../constants/dictionaries';
 import withFieldWrapper from '../hocs/withFieldWrapper';
 import withLocationValues from '../hocs/withLocationValues';
+import styles from '../../styles/index.module.css';
 
 export const HEIGHT = 34;
 
 class Select extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            other: props.settings.userValueAllowed && !props.settings.multiple && props.input.value === (props.settings.userValueQuestion || 'Другое')
+        };
+    }
+
     componentDidMount() {
         if (this.props.dictionaryType) {
             this.fetchDictionary();
@@ -70,8 +79,19 @@ class Select extends Component {
         if (multiple) {
             onChange(data && data.length ? data.map(({ value }) => value) : undefined);
         } else {
-            onChange(path(['value'], data) || undefined);
+            const value = path(['value'], data) || undefined;
+            const userValueQuestion = this.getUserValueQuestion().value;
+
+            onChange(this.allowUserValue() && value === userValueQuestion ? undefined : value);
+            this.allowUserValue() && this.setState({ other: value === userValueQuestion });
         }
+    }
+
+    onChangeOther = e => {
+        this.props.onChange(e.target.value ? {
+            value: 'Иное',
+            userValue: e.target.value,
+        } : undefined);
     }
 
     getOptionsHeight = childrens => {
@@ -104,11 +124,21 @@ class Select extends Component {
         </List>;
     }
 
+    getUserValueQuestion = () => {
+        const value = this.props.settings.userValueQuestion || 'Другое';
+
+        return { label: value, value };
+    }
+
+    allowUserValue = () => {
+        return this.props.settings.userValueAllowed && !this.props.settings.multiple;
+    }
+
     getOptions = () => {
         const { settings, options, formValues } = this.props;
 
         if (formValues && !formValues[settings.regionField] && !formValues[settings.countryField] && !formValues.country || !formValues) {
-            return options;
+            return this.allowUserValue() ? [...options, this.getUserValueQuestion()] : options;
         }
 
         const filteredOptions =  filter(item => {
@@ -128,7 +158,7 @@ class Select extends Component {
             }
         }, options);
 
-        return filteredOptions;
+        return this.allowUserValue() ? [...filteredOptions, this.getUserValueQuestion()] : filteredOptions;
     }
 
     getDropdownIndicator = (props) => {
@@ -158,27 +188,39 @@ class Select extends Component {
         const MenuList = this.getMenuList(options);
         const isError = errors[dictionaryType];
 
-        return <ReactSelect
-            id={name}
-            key={value}
-            value={multiple ? filter(item => contains(item.value, value || []), options) : find(propEq('value', value), options)}
-            options={options}
-            placeholder={placeholder}
-            onChange={this.onChange}
-            isMulti={multiple}
-            isSearchable={options.length > 10}
-            placeholder={null}
-            noOptionsMessage={() => t('noOptionsMessage')}
-            classNamePrefix='jobot-forms'
-            maxMenuHeight={HEIGHT * 6}
-            openMenuOnClick={!isError}
-            components={{
-                MenuList,
-                IndicatorSeparator: () => null,
-                DropdownIndicator: this.getDropdownIndicator,
-            }}
-            isDisabled={disabled}
-            isClearable />;
+        return <Fragment>
+            <ReactSelect
+                id={name}
+                key={value}
+                value={this.allowUserValue() && this.state.other ?
+                    this.getUserValueQuestion() : multiple ?
+                        filter(item => includes(item.value, value || []), options) : find(propEq('value', value), options)
+                }
+                options={options}
+                placeholder={placeholder}
+                onChange={this.onChange}
+                isMulti={multiple}
+                isSearchable={options.length > 10}
+                placeholder={null}
+                noOptionsMessage={() => t('noOptionsMessage')}
+                classNamePrefix='jobot-forms'
+                maxMenuHeight={HEIGHT * 6}
+                openMenuOnClick={!isError}
+                components={{
+                    MenuList,
+                    IndicatorSeparator: () => null,
+                    DropdownIndicator: this.getDropdownIndicator,
+                }}
+                isDisabled={disabled}
+                isClearable />
+            { this.allowUserValue() && this.state.other &&
+                <input
+                    className={`input ${styles.formInput}`}
+                    style={{ marginTop: 10 }}
+                    value={pathOr('', ['userValue'], value)}
+                    onChange={this.onChangeOther} />
+            }
+        </Fragment>;
     }
 }
 
