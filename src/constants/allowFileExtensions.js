123154517adb41adf93c3eb5a78fc3d8867b12a5
Дмитрий Ткачук <tkachuk@experium.ru>
2021-01-14 12:59:35 +0300

    Fix rtf file validation

--- src/constants/allowFileExtensions.js
+++ src/constants/allowFileExtensions.js
@@ -5,7 +5,7 @@ export const defaultAllowFileExtensions = {
         html: 'text/html',
         odt: 'application/vnd.oasis.opendocument.text',
         pdf: 'application/pdf',
-        rtf: 'application/rtf, .rtf',
+        rtf: 'application/rtf, text/rtf, .rtf',
         txt: 'text/plain',
         xls: 'application/vnd.ms-excel',
         xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
@@ -21,7 +21,7 @@ export const TYPES = {
     audio: 'audio/*',
     video: 'video/*',
     image: 'image/jpeg, image/tiff, image/png',
-    document: 'text/plain, application/msword, application/rtf, application/pdf, text/html, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.oasis.opendocument.text, .doc, .docx, .xls, .xlsx, .odt, .rtf, .txt',
+    document: 'text/plain, application/msword, application/rtf, text/rtf, application/pdf, text/html, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.oasis.opendocument.text, .doc, .docx, .xls, .xlsx, .odt, .rtf, .txt',
 };
 
 export const VALIDATION_FILE_TYPES = {
