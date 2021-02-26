32f39f6b04486e466e803aa4425805f6c2d0cd4e
Anastasia Tsegelskaya <muaaaaaaa@mail.ru>
2021-02-26 14:48:02 +0300

    fix multiple native select

--- src/components/formComponents/FormSelect.js
+++ src/components/formComponents/FormSelect.js
@@ -3,12 +3,24 @@ import isMobile from 'ismobilejs';
 import ReactSelect from 'react-select';
 
 import styles from '../../styles/index.module.css';
-import { path } from 'ramda';
+import { filter, path } from 'ramda';
 
 export default class FormSelect extends Component {
     static defaultProps = {
         useNative: true
     };
+
+    onChange = e => {
+        if (this.props.isMulti) {
+            const values = filter(item => item.selected, e.target.options).map(item =>
+                ({ value: isNaN(Number(item.value)) ? item.value : Number(item.value)})
+            );
+
+            this.props.onChange(values.length ? values : null);
+        } else {
+            this.props.onChange({ value: isNaN(Number(e.target.value)) ? e.target.value : Number(e.target.value) });
+        }
+    }
 
     render() {
         const { useNative, nativeStyles, ...props } = this.props;
@@ -17,11 +29,15 @@ export default class FormSelect extends Component {
             <select
                 id={props.id}
                 className={`select-input ${styles.formInput}`}
-                value={path(['value'], props.value) ? String(props.value.value) : undefined}
+                value={props.isMulti ?
+                    (props.value || []).map(({ value }) => String(value)) : path(['value'], props.value) ?
+                        String(props.value.value) : undefined
+                }
                 disabled={props.isDisabled}
                 placeholder={props.placeholder}
-                onChange={e => props.onChange({ value: isNaN(Number(e.target.value)) ? e.target.value : Number(e.target.value) })}
+                onChange={this.onChange}
                 style={nativeStyles}
+                multiple={props.isMulti}
             >
                 <option value=''>Не выбрано</option>
                 { props.options.map((item, index) => (
