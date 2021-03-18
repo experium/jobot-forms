506a728537bbcec1e02b353cb43b621b8fb7763c
Anastasia Tsegelskaya <muaaaaaaa@mail.ru>
2021-03-18 19:28:40 +0300

    fix multiple native select

--- src/components/formComponents/FormSelect.js
+++ src/components/formComponents/FormSelect.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import isMobile from 'ismobilejs';
 import ReactSelect from 'rc-select';
+import { filter, path } from 'ramda';
 
 import styles from '../../styles/index.module.css';
-import { filter, path } from 'ramda';
 
 export default class FormSelect extends Component {
     static defaultProps = {
         useNative: true
     };
 
+    state = {
+        value: null
+    };
+
+    componentDidMount() {
+        this.props.mode === 'multiple' && this.setState({ value: this.props.value });
+    }
+
     onChange = e => {
-        if (this.props.isMulti) {
+        if (this.props.mode === 'multiple') {
             const values = filter(item => item.selected, e.target.options).map(item =>
                 ({ value: isNaN(Number(item.value)) ? item.value : Number(item.value)})
             );
 
-            this.props.onChange(values.length ? values : null);
+            this.setState({ value: values.length ? values : null });
         } else {
             this.props.onChange({ value: isNaN(Number(e.target.value)) ? e.target.value : Number(e.target.value) });
+        }
+    }
+
+    onBlur = () => {
+        if (this.props.mode === 'multiple') {
+            this.props.onChange(this.state.value);
         }
     }
 
@@ -29,7 +43,7 @@ export default class FormSelect extends Component {
             <select
                 id={props.id}
                 className={`select-input ${styles.formInput}`}
-                value={props.isMulti ?
+                defaultValue={props.mode === 'multiple' ?
                     (props.value || []).map(({ value }) => String(value)) : path(['value'], props.value) ?
                         String(props.value.value) : undefined
                 }
@@ -37,9 +51,10 @@ export default class FormSelect extends Component {
                 placeholder={props.placeholder}
                 onChange={this.onChange}
                 style={nativeStyles}
-                multiple={props.isMulti}
+                onBlur={this.onBlur}
+                multiple={props.mode === 'multiple'}
             >
-                { !props.isMulti && <option value=''>Не выбрано</option> }
+                { props.mode !== 'multiple' && <option value=''>Не выбрано</option> }
                 { props.options.map((item, index) => (
                     <option key={item.value || index} value={item.value}>
                         {item.label}
